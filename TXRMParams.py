7b69d97ce772aac766f53668632f99620b5d687a
scmayo <sheridanmayo@gmail.com>
2026-02-13 12:15:56 +1100

    Update TXRMParams.py
    Added brief commentary

--- TXRMParams.py
+++ TXRMParams.py
@@ -4,10 +4,9 @@ Created on Mon Feb  9 15:51:08 2026
 
 @author: may130 - Heavily modified from Yakov's code'
 """
-import olefile
-from datetime import datetime
-import numpy as np
+#This file contain tools for extracting metadata from a TXRM file which is an OLE format file of tomography scan data produced by Xradia Tomography instruments
 
+#The five functions below are helper functions for extracting data from ole streams in various formats
 
 def GetFloatAttr(ole, string: str):
     if ole.exists(string):    
@@ -61,107 +60,114 @@ def GetDate(ole, string: str):
         return np.array([])
 
 def SampleParams(strFile: str, Sampleno: str, Altime: int = 15, Proctime: int = 15):
+    #This function extracts the desired subset of the metadata from a TXRM file using the olefile tools and 
+    #returns them in a formatted form as "output" them along with a summary of the output "feedback" for
+    #sending to console or a GUI
+    
     if strFile == "":
         print("Missing filename! Terminating...")
         return None
         
     outstring = ""
     data = ""
+    feedback = ""
     
     from pathlib import Path
     my_file = Path(strFile)
 
     if not my_file.is_file():
-        print("File does not exist! Terminating...")
-        return None
+        feedback+=("File does not exist! Terminating...")
+        return None,feedback
 
     if (str(Path(strFile).suffix) != ".txrm"):
-        print("File not a .txrm file! Terminating...")
-        return None
+        feedback+=("File not a .txrm file! Terminating...")
+        return None,feedback
 
 
     if olefile.isOleFile(strFile):
         with olefile.OleFileIO(strFile, write_mode=True) as ole:
-            print("Opening file...")
+            #feedback+=("Opening file...")
             
             # Extract required parameters
             Voltage = GetFloatAttr(ole, 'ImageInfo/Voltage')
             if len(Voltage) == 0:
                 print('Could not find voltage. Terminating...')
-                return None
-            print(f'Voltage: {Voltage[0]} kV')
+                return None,feedback
+            feedback+=(f'\nVoltage: {Voltage[0]} kV')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Voltage,"+f"{Voltage[0]:.0f}\n")
 
             Power = GetFloatAttr(ole, 'AcquisitionSettings/SrcPower')
             if len(Power) == 0:
-                print('Could not find Power. Terminating...')
-                return None
-            print(f'Power: {Power[0]} W')
+                feedback+=('Could not find Power. Terminating...')
+                return None,feedback
+            feedback+=(f'\nPower: {Power[0]} W')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Power,"+f"{Power[0]:.0f}\n")
 
             Filter = GetText(ole, 'AcquisitionSettings/SourceFilterName')
             if len(Filter) == 0:
-                print('Could not find Filter. Terminating...')
-                return None
-            #print("Filter: "+Filter.decode('ascii'))
+                feedback+=('Could not find Filter. Terminating...')
+                return None,feedback
+            feedback+=("\nFilter: "+Filter.decode('ascii'))
             data = Filter.decode('ascii')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Source Filter,"+data+"\n")
 
             FramesPerImage = GetIntAttr(ole, 'AcquisitionSettings/FramesPerImage')
             if len(FramesPerImage) == 0:
-                print('Could not find FramesPerImage. Terminating...')
-                return None
-            print(f'Frames per Image: {FramesPerImage[0]:.0f}')
+                feedback+=('Could not find FramesPerImage. Terminating...')
+                return None,feedback
+            feedback+=(f'\nFrames per Image: {FramesPerImage[0]:.0f}')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Frames per image,"+f"{FramesPerImage[0]:.0f}\n")
             
             
             ExpTime = GetFloatAttr(ole, 'AcquisitionSettings/ExpTime')
             if len(ExpTime) == 0:
-                print('Could not find ExpTime. Terminating...')
-                return None
-            print(f'Exp Time: {ExpTime[0]:.1f} mm')
+                feedback+=('Could not find ExpTime. Terminating...')
+                return None,feedback
+            feedback+=(f'\nExp Time: {ExpTime[0]:.1f} mm')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Exposure time,"+f"{ExpTime[0]:.1f}\n")
             
             ObjName = GetText(ole, 'ImageInfo/ObjectiveName')
             if len(ObjName) == 0:
-                print('Could not find ObjName. Terminating...')
-                return None
-            print("Detector-to-RA distance:"+ObjName.decode('ascii'))
-            outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Source Filter,"+ObjName.decode('ascii')+"\n")
+                feedback+=('Could not find ObjName. Terminating...')
+                return None,feedback
+            feedback+=("\nDetector:"+ObjName.decode('ascii'))
+            outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Objective,"+ObjName.decode('ascii')+"\n")
 
             Binning = GetIntAttr(ole, 'AcquisitionSettings/Binning')
             if len(Binning) == 0:
-                print('Could not find Binning. Terminating...')
-                return None
-            print(f'Binning: {Binning[0]:.0f}')
+                feedback+=('Could not find Binning. Terminating...')
+                return None,feedback
+            feedback+=(f'\nBinning: {Binning[0]:.0f}')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Binning,"+f"{Binning[0]:.0f}\n")
             
-            NumStitch = GetIntAttr(ole, 'ReconSettings/StitchParams/AutoStitchSettings/NumSegments')
+            NumStitch = GetIntAttr(ole, 'AcquisitionSettings/StitchParams/AutoStitchSettings/NumSegments')
             if len(NumStitch) == 0:
-                print('Could not find NumStitch. Terminating...')
-                return None
-            print(f'NumStitch: {NumStitch[0]}')
-            outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Number of stitches,"+f"{NumStitch[0]:.0f}\n")
+                feedback+=('\nCould not find NumStitch - stitch = 1.')
+                outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Number of stitches,1\n")
 
+            else:
+                feedback+=(f'\nNumStitch: {NumStitch[0]}')
+                outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Number of stitches,"+f"{NumStitch[0]:.0f}\n")
+            
             NumImages = GetIntAttr(ole, 'ImageInfo/NoOfImages')
             if len(NumImages) == 0:
-                print('Could not find Num Images. Terminating...')
-                return None
-            print(f'Num Images: {NumImages[0]}')
+                feedback+=('Could not find Num Images. Terminating...')
+                return None,feedback
+            feedback+=(f'\nNum Images: {NumImages[0]}')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Projections,"+f"{NumImages[0]:.0f}\n")
             
             PixSize = GetFloatAttr(ole, 'ImageInfo/PixelSize')
             if len(PixSize) == 0:
-                print('Could not find PixelSize. Terminating...')
-                return None
-            print(f'Pix Size: {PixSize[0]:.2f} um')
+                feedback+=('Could not find PixelSize. Terminating...')
+                return None,feedback
+            feedback+=(f'\nPix Size: {PixSize[0]:.2f} um')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Pixel Size,"+f"{PixSize[0]:.2f}\n")
 
             DateTime = GetDate(ole, 'ImageInfo/Date')
             if len(DateTime) == 0:
-                print('Could not find DateTime. Terminating...')
-                return None
-            print("Start, end time:"+DateTime[0].decode('ascii')+DateTime[1].decode('ascii'))
+                feedback+=('Could not find DateTime. Terminating...')
+                return None,feedback
+            feedback+=("\nStart, end time:"+DateTime[0].decode('ascii')+DateTime[1].decode('ascii'))
             format_string = "%m/%d/%Y %H:%M:%S"
             t1= datetime.strptime(DateTime[0].decode('ascii'), format_string)
             t2 = datetime.strptime(DateTime[1].decode('ascii'), format_string)
@@ -172,17 +178,17 @@ def SampleParams(strFile: str, Sampleno: str, Altime: int = 15, Proctime: int = 
 
             BHparam = GetFloatAttr(ole, 'ReconSettings/BeamHardening')
             if len(BHparam) == 0:
-                print('Could not find BHparam. Terminating...')
-                return None
-            print(f'BH param: {BHparam[0]:.2f} um')
+                feedback+=('Could not find BHparam. Terminating...')
+                return None,feedback
+            feedback+=(f'\nBH param: {BHparam[0]:.2f} um')
             outstring+=("0,"+Sampleno+",Global,Scanning Parameters,Beam Hardening,"+f"{BHparam[0]:.2f}\n")
 
             VLTmode = GetText(ole, 'ReconSettings/BeamHardeningFileName')
             if len(VLTmode) == 0:
-                print('Could not find VLTmode. Terminating...')
-                return None
+                feedback+=('\nCould not find VLTmode. Terminating...')
+                return None,feedback
             temp = VLTmode.decode('ascii')
-            print("VLTmode:"+temp)
+            feedback+=("\nVLTmode:"+temp)
             vltyes = "FALSE"
             if (temp == "BH Correction for Very Low Transmission"):
                 vltyes="TRUE"
@@ -194,16 +200,16 @@ def SampleParams(strFile: str, Sampleno: str, Altime: int = 15, Proctime: int = 
         #print(outstring)
         
     else:
-        print("Unsupported file format. Terminating...")
-    return outstring
+        feedback+=("Unsupported file format. Terminating...")
+    return outstring,feedback
 
 def MakeTable(strFile: str, SampleNos, Altime: int = 15, Proctime: int = 15):
+    #This function creates a formatted list of (the same) metadata for multiple samples suitable for importing into our LIMS
+    
     outtable = ",Sample ID,Phase,Analysis,Component Name,Value\n"
-    params = SampleParams(strFile,"0000")
+    params, feedback = SampleParams(strFile,"0000", Altime, Proctime)
     if params == None:
-        return None
+        return None, feedback
     for i in SampleNos:
-        outtable+=params.replace("0000",str(i))
-        
-    return outtable
-
+        outtable+=params.replace("0000",str(i))        
+    return outtable,feedback
